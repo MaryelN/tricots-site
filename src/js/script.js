022497257f034dde20649ce13a5438c14289cb14
MaryelN <maryel.navarromtz@gmail.com>
2023-12-01 00:36:52 +0100

    add of function for numbers

--- src/js/script.js
+++ src/js/script.js
@@ -13,5 +13,26 @@ window.onscroll = () => {
       sec.classList.remove('show-animate');
     }
     })
-
 }
+
+let valueDisplays = document.querySelectorAll('.numbers');
+let interval = 1000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute('data-val'), 10);
+  console.log(`endValue for this element: ${endValue}`);
+
+  let duration = Math.floor(interval / (endValue - startValue));
+
+  let counter = setInterval(() => {
+    if (startValue <= endValue) {
+      valueDisplay.textContent = startValue;
+      startValue += 1;
+    } else {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+window.onscroll = startAnimationIfVisible;
+document.addEventListener('DOMContentLoaded', startAnimationIfVisible);
